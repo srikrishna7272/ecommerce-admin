93a3e2fb0e5e948405418d159a4173456aae6a67
srikrishna7272 <105886453+srikrishna7272@users.noreply.github.com>
2023-10-27 14:56:03 -0700

    built authentication using clerk

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,3 @@
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
+  return <div className="flex h-full justify-center items-center">{children}</div>;
+}
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <div className="p-4"></div>;
+  return <div className="p-4">dsdsd</div>;
 }
