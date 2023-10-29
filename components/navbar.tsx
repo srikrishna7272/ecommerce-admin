62c614b74f86b686b1ca48ee96befe751ee593ef
srikrishna7272 <105886453+srikrishna7272@users.noreply.github.com>
2023-10-29 15:20:40 -0700

    built main navbar outline.

---
+++ components/navbar.tsx
@@ -0,0 +1,19 @@
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
+import { MainNav } from "./main-nav";
+
+const Navbar = () => {
+  return (
+    <div className="border-b">
+      <div className="flex h-16 items-center px-4">
+        <div>Store switcher</div>
+        <MainNav className="mx-6" />
+        <div className="ml-auto flex items-center space-x-4">
+          <UserButton afterSignOutUrl="/" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
