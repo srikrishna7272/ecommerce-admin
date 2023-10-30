a16a9df9955491774a4049d65b7a0d04579c91ba
srikrishna7272 <105886453+srikrishna7272@users.noreply.github.com>
2023-10-30 11:22:25 -0700

    commiting

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -1,5 +1,5 @@
 const BillboardPage = () => {
-  return <div>This is a form for billboards.</div>;
+  return <div>This is a form for billboards. jsafjndsfjkdfc</div>;
 };
 
 export default BillboardPage;
